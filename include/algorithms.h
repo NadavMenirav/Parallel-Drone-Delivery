a2a2d428472ede780b8952115e09ff82ac37c090
AstroTomer <tomergueta99@gmail.com>
2026-03-05 20:16:18 +0200

    comments

--- include/algorithms.h
+++ include/algorithms.h
@@ -3,10 +3,15 @@
 
 #include "entities.h" 
 
+// This header file contains the function prototypes for the algorithms used in the delivery system.
+
+// Calculate Euclidean distance (Pythagorean theorem)
 double calculateDistance(Position p1, Position p2);
 
+// Calculate flight time according to the formula t = d / v
 double calculateFlightTime(double distance, double velocity);
 
+// Update the priorities of customers based on whether they have been served or not
 void updateCustomerPriorities(Customer* customers, int customerCount);
 
 // Calculates the priority-aware optimization score for a customer-drone assignment

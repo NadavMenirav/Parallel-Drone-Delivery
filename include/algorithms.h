dfdff0c9d29c3d61893256e27460c399cb12c41e
AstroTomer <tomergueta99@gmail.com>
2026-03-07 12:38:07 +0200

    first pic

--- include/algorithms.h
+++ include/algorithms.h
@@ -17,4 +17,13 @@ void updateCustomerPriorities(Customer* customers, int customerCount);
 // Calculates the priority-aware optimization score for a customer-drone assignment
 double calculateCustomerScore(int priority, double estimatedTime);
 
+// Creates and computes a 2D distance matrix [Customer][Bakery] in parallel
+double** calculateDistanceMatrix(Bakery* bakeries, int bakeryCount, Customer* customers, int customerCount);
+
+// Frees the memory allocated for the distance matrix
+void freeDistanceMatrix(double** matrix, int customerCount);
+
+// Processes customers who have been served, deciding if they leave or order again
+void processCustomerTransitions(Customer* customers, int customerCount);
+
 #endif
